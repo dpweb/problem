70abf41a01b03632251795eb31922a3660c480a9
digital labs <success@dpsw.info>
2014-01-01 11:05:12 -0600

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 var problem = require('./problem.js');
 
-problem.email('me@dpsw.info', 'me@dpsw.info', 'subject', 'body');
-problem.sms('18477540908', 'body');
+problem.email('me@dps.info', 'me@dps.info', 'subject', 'body');
+problem.sms('12477540308', 'body');
